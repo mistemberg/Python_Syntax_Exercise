e9598db8f851daf3ca73cc902ca5595ac38bc4ca
mistemberg <101603348+mistemberg@users.noreply.github.com>
2023-07-02 17:40:32 -0400

    Update any7.py

--- any7.py
+++ any7.py
@@ -2,10 +2,11 @@ def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
 
     # YOUR CODE HERE 
-    if nums === 7
-    print("True")
-    else
-    print("False")
+    for num in nums:
+        if num == 7:
+            return True
+
+    return False
 
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
